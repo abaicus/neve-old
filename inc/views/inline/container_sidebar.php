5aa2ab5d1981ee2a8eb5eb77c95cb9449ce360e5
Andrei Baicus <baicusandrei@me.com>
2018-10-08 18:07:23 +0300

    feat: [wip] gutenberg aligments compatibility

--- inc/views/inline/container_sidebar.php
+++ inc/views/inline/container_sidebar.php
@@ -30,6 +30,10 @@ class Container_Sidebar extends Base_Inline {
 	private function container_style() {
 		$container_width = get_theme_mod( 'neve_container_width' );
 		$container_width = json_decode( $container_width, true );
+		if( empty( $container_width ) ) {
+			return;
+		}
+
 		$settings        = array(
 			array(
 				'css_prop' => 'max-width',
@@ -37,7 +41,29 @@ class Container_Sidebar extends Base_Inline {
 				'suffix'   => 'px',
 			),
 		);
-		$this->add_responsive_style( $settings, '.container' );
+		$selectors       = '.container, 
+		.nv-single-post-wrap > *:not(.nv-content-wrap), 
+		.nv-single-post-wrap .nv-content-wrap > *:not(.alignwide):not(.alignfull), 
+		.nv-single-page-wrap > *:not(.nv-content-wrap), 
+		.nv-single-page-wrap .nv-content-wrap > *:not(.alignwide):not(.alignfull)';
+
+		$this->add_responsive_style( $settings, $selectors );
+
+		$alignwide_width = $container_width; //+100px
+		array_walk( $alignwide_width, function ( &$item1 ) {
+			$item1 += 100;
+		} );
+
+		$settings = array(
+			array(
+				'css_prop' => 'max-width',
+				'value'    => $alignwide_width,
+				'suffix'   => 'px',
+			),
+		);
+
+		$this->add_responsive_style( $settings, '.alignwide' );
+
 	}
 
 	/**
@@ -49,15 +75,15 @@ class Container_Sidebar extends Base_Inline {
 		$content_width = 100 - $sidebar_width;
 		$settings      = array(
 			'content' => array(
-				'css_prop' => 'max-width',
+				'css_prop' => 'max - width',
 				'value'    => $content_width,
-				'suffix'   => '%',
+				'suffix'   => ' % ',
 			),
 			'sidebar' => array(
 				array(
-					'css_prop' => 'max-width',
+					'css_prop' => 'max - width',
 					'value'    => $sidebar_width,
-					'suffix'   => '%',
+					'suffix'   => ' % ',
 				),
 			),
 		);
